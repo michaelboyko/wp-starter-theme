d4734c40e8ac5ff6febc0156689ff27ce2cc3b69
michaelboyko <michael@michaelboyko.ca>
2022-11-24 11:26:41 -0500

    feat: removed unneeded css

--- assets/src/js/custom/common.js
+++ assets/src/js/custom/common.js
@@ -30,11 +30,6 @@ const common = (($) => {
 		// Custom Formidable Checkboxes
 		$('.frm_checkbox label').append('<span class="checkmark"></span>');
 
-		// Trigger Close On Fancy Menu
-		$(".fancy-menu-header-close button").click(function(event) {
-			$("#menu-toggle-close").trigger("click");
-		});
-
 	};
 
 	return {
